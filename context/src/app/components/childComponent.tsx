232b6c9985d45d477dcd5bfee14dc242b80c2a91
pinarinemek <77541682+pinarinemek@users.noreply.github.com>
2023-12-14 20:35:22 +0300

    Update childComponent.tsx

--- context/src/app/components/childComponent.tsx
+++ context/src/app/components/childComponent.tsx
@@ -5,7 +5,8 @@ import ChildComponentSecond from "./childComponentSecond";
 export default function ChildComponent() {
   const { data } = useAppContext();
   return (
-    <div style={{height:"700px", borderWidth:"5px",borderColor:"red", borderStyle:"solid", textAlign:"center",padding:"2%"}}>
+    <div style={{height:"700px", borderWidth:"5px",borderColor:"red", 
+                 borderStyle:"solid", textAlign:"center",padding:"2%"}}>
       <h3>Child Component</h3>
       <p>This is a child component</p>
       <p>Data : {data}</p>
